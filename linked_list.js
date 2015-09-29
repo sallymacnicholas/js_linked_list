007e5bd9840d559f745ea85bf2b6483f8d12422e
Sally MacNicholas <smacnicholas@gmail.com>
2015-09-29 11:24:22 -0600

    first commit

--- linked_list.js
+++ linked_list.js
@@ -2,6 +2,85 @@ function createList() {
   return { head: null };
 }
 
-function push(list, data) {
-  // your code here :)
+function append(list, data) {
+  if (list.head === null) {
+    list.head = new node(data);
+  }
+  else {
+    findTail(list).nextNode = new node(data);
+  }
+}
+
+function node(data) {
+  this.data = data;
+  this.nextNode = null;
+}
+
+function prepend(list, data) {
+  if (list.head === null) {
+    list.head = new node(data);
+  }
+  else {
+    
+  }
+}
+
+function findTail(list) {
+  var node = list.head
+  while(node.nextNode !== null) {
+    node = node.nextNode;
+  }
+  return node;
+}
+
+function tailValue(list) {
+  return findTail(list).data
+}
+
+function headValue(list) {
+  return list.head.data
+}
+
+function count(list) {
+  var count = 0;
+  if (list.head !== null) {
+    var node = list.head
+    var count = 1
+    while(node.nextNode !== null) {
+      count ++
+      node = node.nextNode
+    }
+  }
+  return count;
+};
+
+function pop(list) {
+  if (list.head.nextNode === null) {
+    list.head = null;
+  }
+  else {
+    var node = list.head;
+    while(node.nextNode.nextNode !== null) {
+      node = node.nextNode;
+    }
+    node.nextNode = null;
+  }
+}
+
+function include(list, string) {
+  var node = list.head;
+  if (list.head.data === string) {
+    return true;
+  }
+  else {
+    while(node.nextNode !== null) {
+      if (node.data.includes(string) === true) {
+        return true;
+      }
+      else {
+        node = node.nextNode
+        return false;
+      }
+    }
+  }
 }
--- linked_list_test.js
+++ linked_list_test.js
@@ -8,7 +8,48 @@ describe('testing', function () {
 
   it('appends an item to the list', function() {
     var list = createList();
-    push(list, "pizza");
+    append(list, "pizza");
     assert.equal(list.head.data, "pizza");
+    append(list, "pepperoni");
+  });
+
+  it('appends more items to the list', function() {
+    var list = createList();
+    append(list, "pizza");
+    append(list, "pepperoni");
+    assert.equal(list.head.data, "pizza");
+    assert.equal(list.head.nextNode.data, "pepperoni");
+  });
+
+  it ('knows the head of a list', function () {
+    var list = createList();
+    append(list, "pizza");
+    append(list, "pepperoni");
+    assert.equal(headValue(list), "pizza");
+  });
+
+  it ('knows the tail of a list', function() {
+    var list = createList();
+    append(list, "pizza");
+    append(list, "pepperoni");
+    assert.equal(tailValue(list), "pepperoni");
+  });
+
+  it ('can count items in a list', function () {
+    var list = createList();
+    assert.equal(count(list), 0);
+    append(list, "pizza");
+    assert.equal(count(list), 1)
+    append(list, "pepperoni");
+    assert.equal(count(list), 2)
+  });
+
+  it ('can pop off the last item of a list', function () {
+    var list = createList();
+    append(list, "pizza");
+    append(list, "pepperoni");
+    assert.equal(count(list), 2);
+    pop(list);
+    assert.equal(count(list), 1);
   });
 });
